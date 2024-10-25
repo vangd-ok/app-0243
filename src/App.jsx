bbf12ec669dfabf91f49f02941c0f61b61bc8aa7
BulanovaKsenia <bulanovaksenia@gmail.com>
2024-10-25 08:02:45 +0300

    repair all

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { NavLink, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
 
-const profile = () => {
-  return <h3>Страница с профилем</h3>
-}
-const friends = () => {
-  return <h3>Страница друзей</h3>
-}
+const Profile = () => {
+  return <h3>Страница с профилем</h3>;
+};
+const Friends = () => {
+  return <h3>Страница друзей</h3>;
+};
 
 function App() {
   return (
     <div className="container-fluid pl-5 pr-5 mt-5">
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="nav flex-column nav-pills">
-            <NavLink to="/" className="nav-link-active">Главная</NavLink>
-            <NavLink to="profile" className="nav-link">Профиль</NavLink>
-            <NavLink to="friends" className="nav-link">Друзья</NavLink>
-            <NavLink to="shop" className="nav-link">Магазин</NavLink>
-            <NavLink to="settings" className="nav-link">Настройки</NavLink>
-          </div>
-        </div>
-        <div className="col-sm-8">
-          <Routes>
-            <Route path="/" element = {<h2>Главная страница, выберите из меню слева</h2>}/>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/friends" element={<Friends />} />
-          </Routes>
+    <div className="row">
+      <div className="col-sm-4">
+        <div className="nav flex-column nav-pills">
+          <NavLink to="/" className="nav-link">
+            Главная
+          </NavLink>
+          <NavLink to="profile" className="nav-link">
+            Профиль
+          </NavLink>
+          <NavLink to="friends" className="nav-link">
+            Друзья
+          </NavLink>
+          <NavLink to="shop" className="nav-link">
+            Магазин
+          </NavLink>
+          <NavLink to="settings" className="nav-link">
+            Настройки
+          </NavLink>
         </div>
       </div>
+      <div className="col-sm-8">
+        <Routes>
+          <Route path="/" element={<h2>Главная страница, выберите из меню слева</h2>}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/friends" element={<Friends />}/>
+        </Routes>
+      </div>
     </div>
+  </div>
   )
 }
 
