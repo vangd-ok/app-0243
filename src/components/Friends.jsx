1be0e44110b7b5fa535b465a62f71f9d8efd10e3
BulanovaKsenia <bulanovaksenia@gmail.com>
2024-10-29 10:27:42 +0300

    call server, add class Friends

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import friendsImg from "../assets/img/s-l400.jpg"
+import React from "react";
 
 const TableRow = (props) => {
   return (
@@ -15,36 +17,52 @@ const TableRow = (props) => {
   );
 }
 
+export class Friends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {userRow: [] };
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="rou">
+          <div className="col-12">
+            <h3>Список друзей</h3>
+            <p>Доп инфо</p>
+            <img src={friendsImg} alt="" width="30%"/>
+          </div>
+        </div>
+        <div className="row">
+        <table className="table table-bordered">
+          <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Имя и Фамилия</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+        </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 export const Friends = (props) => {
   let users = props.function();  
+
   let usersCount = Object.keys(users).length;
   let userRow = [];
   for (let i = 0; i < usersCount; i++) {
     userRow.push(<TableRow key={i} index={i} id={users[i].id} name={users[i].name} lastname={users[i].lastname} email={users[i].email}/>);
   }
-  return (
-    <div className="container-fluid">
-      <div className="rou">
-        <div className="col-12">
-          <h3>Список друзей</h3>
-          <p>Доп инфо</p>
-        </div>
-      </div>
-      <div className="row">
-      <table className="table table-bordered">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Имя и Фамилия</th>
-          <th scope="col">Email</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        {userRow}
-      </tbody>
-      </table>
-      </div>
-    </div>
-  );
+  
 };
--- src/state.jsx
+++ src/state.jsx
@@ -7,21 +7,25 @@ const user = {
   avatar: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?fm=jpg&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D&w=300&q=80"
 };
 
-const users = {
-  0: {name: "Вано", lastname: "Смиронидзе", id: 4, email: "vs@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
-  1: {name: "Кира", lastname: "Петрова", id: 77, email: "kp@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
-  2: {name: "Маша", lastname: "Сидорова", id: 15, email: "ms@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
-  3: {name: "Петя", lastname: "Петухов", id: 3, email: "pp@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80" },
-  4: {name: "Глаша", lastname: "Коровина", id: 40, email: "gk@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80" },
-}
+// const users = {
+//   0: {name: "Вано", lastname: "Смиронидзе", id: 4, email: "vs@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
+//   1: {name: "Кира", lastname: "Петрова", id: 77, email: "kp@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
+//   2: {name: "Маша", lastname: "Сидорова", id: 15, email: "ms@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnN8ZW58MHx8MHx8fDA%3D&w=300&q=80" },
+//   3: {name: "Петя", lastname: "Петухов", id: 3, email: "pp@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80" },
+//   4: {name: "Глаша", lastname: "Коровина", id: 40, email: "gk@mail.ru", about: "тут я раскажу о себе", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80" },
+// }
 
-export function getUser(userId) {
-  for (let i = 0; i < Object.keys(users).length; i++) {
-    if (users[i].id == userId) return users[i];
-  }
+let users = [];
+export function getUser() {
+  // for (let i = 0; i < Object.keys(users).length; i++) {
+  //   if (users[i].id == userId) return users[i];
+  // }
   return user;
 }
 
-export function getUsers() {
+export async function getUsers() {
+  let response = await fetch("http://feer.p-host.in/getUsers");
+  users = await response.json();
+  console.log(users);
   return users;
 }
